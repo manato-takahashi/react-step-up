e652f2eef7d42c10a9d2ea06aff03adaeba6bc44
Manato Takahashi <manato.takahashi86@gmail.com>
2025-02-05 17:21:10 +0900

    add: Emotionまで完了し、さまざまなCSSの当て方を完了

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import { InlineStyle } from './components/InlineStyle';
+import { StyledJsx } from './components/StyledJsx';
+import { StyledComponents } from './components/StyledComponents';
+import { Emotion } from './components/Emotion';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,6 +14,9 @@ function App() {
     <>
       <div className="App">
         <InlineStyle />
+        <StyledJsx />
+        <StyledComponents />
+        <Emotion />
       </div>
     </>
   );
